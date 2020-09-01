b2d9cdd6f7444a23f3ec1caa44a1e23ed22fa506
FalconRay <falconray@yahoo.com>
2020-09-01 15:48:40 +0800

    Added multithread download command into network.lib

--- network_test.sh
+++ network_test.sh
@@ -10,46 +10,13 @@ set -e
 . ./echo_color.lib
 . ./network.lib
 
-usage_func()
-{
-    echo "Usage:"
-    echo './network_test.sh -i eth0'
-}
 
-
-no_args="true"
-INTERFACE_NAME="eth0"
-while getopts "i:p:" opts
-do
-    case $opts in
-        i)
-              # ip list
-              INTERFACE_NAME=$OPTARG
-              ;;
-        :)
-            echo "The option -$OPTARG requires an argument."
-            exit 1
-            ;;
-        ?)
-            echo "Invalid option: -$OPTARG"
-            usage_func
-            exit 2
-            ;;
-        *)    #unknown error?
-              echoR "unkonw error."
-              usage_func
-              exit 1
-              ;;
-    esac
-    no_args="false"
-done
-
-[[ "$no_args" == "true" ]] && { usage_func; exit 1; }
-
+INTERFACE_NAME="enx9cebe8105d32"
 IP=$(get_iether_IP4 $INTERFACE_NAME)
 echoY ${IP}
 
 MAC=$(get_iether_MAC $INTERFACE_NAME)
 echoY ${MAC}
 
+${DOWNLOAD_RESOURCE} -o test.png https://www.baidu.com/img/flexible/logo/pc/result.png
 
