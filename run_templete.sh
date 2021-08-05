4987d0bef8777deadab48b929c1d691d60a45ead
falconray0704 <falconray@yahoo.com>
2021-08-05 15:08:26 +0800

    Refined run_templete.sh with running environment informations checking. (#2)
    * Making run_templete.sh to check .env_setup symbolic for running environment informations setup.
    
    * Ignore the symbolic link .env_setup for commiting.

--- run_templete.sh
+++ run_templete.sh
@@ -15,7 +15,16 @@ export LIBSHELL_ROOT_PATH=${PWD}
 . ${LIBSHELL_ROOT_PATH}/echo_color.lib
 . ${LIBSHELL_ROOT_PATH}/utils.lib
 . ${LIBSHELL_ROOT_PATH}/sysEnv.lib
-. ./.env_setup
+
+# Checking environment setup symbolic link and its file exists
+if [ -L ".env_setup" ] && [ -f ".env_setup" ]
+then
+#    echoG "Symbolic .env_setup exists."
+    . ./.env_setup
+else
+    echoR "Setup environment informations by making .env_setup symbolic link to specific .env_setup_xxx file(eg: .env_setup_amd64_ubt_1804) ."
+    exit 1
+fi
 
 
 
