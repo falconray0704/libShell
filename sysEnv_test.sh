3ce5d5dce72363936f1fe89b085b9437793de33d
FalconRay <falconray0704@yahoo.com>
2022-02-22 03:40:42 -0800

    Added raspbian32 11, armv7l environment support.

--- sysEnv_test.sh
+++ sysEnv_test.sh
@@ -3,7 +3,8 @@
 set +x
 set -e
 
-#. .env_setup
+export LIBSHELL_ROOT_PATH=${PWD}
+. .env_setup
 . ./echo_color.lib
 . ./sysEnv.lib
 
@@ -29,9 +30,19 @@ echoY "docker tag cpu arch: ${OSENV_DOCKER_TAG_CPU_ARCH}"
 VAL_ARM_VERSION=$(arm_version)
 if [ -z ${VAL_ARM_VERSION} ]
 then
-    echoR "CPU arch is not ARM!!!"
+    echoR "CPU arch ${VAL_ARM_VERSION} not ARM!!!"
 else
     echoY "CPU arm version: ${VAL_ARM_VERSION}"
+fi
+
+
+if [ $(is_Ubuntu_x86_64_bionic) -eq 1 ] || \
+    [ $(is_Pi3BP_Raspbian_aarch64_Debian_bullseye) -eq 1 ] || \
+    [ $(is_Pi3BP_Raspbian_armv7l_Debian_bullseye) -eq 1 ]
+then
+    echoY "Platform info: OSENV_DIST_ID == ${OSENV_DIST_ID}, OSENV_DIST_CODENAME == ${OSENV_DIST_CODENAME}, OSENV_OS_CPU_ARCH == ${OSENV_OS_CPU_ARCH}"
+else
+    echoR "Unsupported Platform : OSENV_DIST_ID == ${OSENV_DIST_ID}, OSENV_DIST_CODENAME == ${OSENV_DIST_CODENAME}, OSENV_OS_CPU_ARCH == ${OSENV_OS_CPU_ARCH}"
 fi
 
 is_root_func
